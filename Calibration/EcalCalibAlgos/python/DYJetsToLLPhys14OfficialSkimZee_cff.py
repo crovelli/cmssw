deeab109975c6e5fffa227e4eecfa5a1598b47ce
Chiara Rovelli <crovelli@gmai.com>
2015-07-02 16:25:03 +0200

    first commit

---
+++ Calibration/EcalCalibAlgos/python/DYJetsToLLPhys14OfficialSkimZee_cff.py
@@ -0,0 +1,21 @@
+import FWCore.ParameterSet.Config as cms
+
+readFilesRM = cms.untracked.vstring()
+readFilesRM.extend( [
+
+"/store/user/crovelli/zeeCalib/DYJetsToLL_M-50_13TeV-madgraph-pythia8/crab_DYJetsToLL_Phys14DR_zeeSkim/150701_105841/0000/zeeCalibSkim_1.root",
+"/store/user/crovelli/zeeCalib/DYJetsToLL_M-50_13TeV-madgraph-pythia8/crab_DYJetsToLL_Phys14DR_zeeSkim/150701_105841/0000/zeeCalibSkim_10.root",
+"/store/user/crovelli/zeeCalib/DYJetsToLL_M-50_13TeV-madgraph-pythia8/crab_DYJetsToLL_Phys14DR_zeeSkim/150701_105841/0000/zeeCalibSkim_11.root",
+"/store/user/crovelli/zeeCalib/DYJetsToLL_M-50_13TeV-madgraph-pythia8/crab_DYJetsToLL_Phys14DR_zeeSkim/150701_105841/0000/zeeCalibSkim_12.root",
+"/store/user/crovelli/zeeCalib/DYJetsToLL_M-50_13TeV-madgraph-pythia8/crab_DYJetsToLL_Phys14DR_zeeSkim/150701_105841/0000/zeeCalibSkim_13.root",
+"/store/user/crovelli/zeeCalib/DYJetsToLL_M-50_13TeV-madgraph-pythia8/crab_DYJetsToLL_Phys14DR_zeeSkim/150701_105841/0000/zeeCalibSkim_14.root",
+"/store/user/crovelli/zeeCalib/DYJetsToLL_M-50_13TeV-madgraph-pythia8/crab_DYJetsToLL_Phys14DR_zeeSkim/150701_105841/0000/zeeCalibSkim_2.root",
+"/store/user/crovelli/zeeCalib/DYJetsToLL_M-50_13TeV-madgraph-pythia8/crab_DYJetsToLL_Phys14DR_zeeSkim/150701_105841/0000/zeeCalibSkim_3.root",
+"/store/user/crovelli/zeeCalib/DYJetsToLL_M-50_13TeV-madgraph-pythia8/crab_DYJetsToLL_Phys14DR_zeeSkim/150701_105841/0000/zeeCalibSkim_4.root",
+"/store/user/crovelli/zeeCalib/DYJetsToLL_M-50_13TeV-madgraph-pythia8/crab_DYJetsToLL_Phys14DR_zeeSkim/150701_105841/0000/zeeCalibSkim_5.root",
+"/store/user/crovelli/zeeCalib/DYJetsToLL_M-50_13TeV-madgraph-pythia8/crab_DYJetsToLL_Phys14DR_zeeSkim/150701_105841/0000/zeeCalibSkim_6.root",
+"/store/user/crovelli/zeeCalib/DYJetsToLL_M-50_13TeV-madgraph-pythia8/crab_DYJetsToLL_Phys14DR_zeeSkim/150701_105841/0000/zeeCalibSkim_7.root",
+"/store/user/crovelli/zeeCalib/DYJetsToLL_M-50_13TeV-madgraph-pythia8/crab_DYJetsToLL_Phys14DR_zeeSkim/150701_105841/0000/zeeCalibSkim_8.root",
+"/store/user/crovelli/zeeCalib/DYJetsToLL_M-50_13TeV-madgraph-pythia8/crab_DYJetsToLL_Phys14DR_zeeSkim/150701_105841/0000/zeeCalibSkim_9.root"
+
+] )
