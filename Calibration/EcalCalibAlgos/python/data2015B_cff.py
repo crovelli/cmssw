d0516f0ba1fa6ff8d76b37975754c8218bbf1c67
Chiara Rovelli <crovelli@gmai.com>
2015-07-14 10:38:43 +0200

    first commit

---
+++ Calibration/EcalCalibAlgos/python/data2015B_cff.py
@@ -0,0 +1,23 @@
+import FWCore.ParameterSet.Config as cms
+
+readFilesRM = cms.untracked.vstring() 
+readFilesRM.extend( [   
+
+"/store/user/crovelli/zeeCalib/2012B/DoubleEG_bis/DoubleEG/crab_ZSkimData2012B/150713_203921/0000/zeeCalibSkim_10.root",
+"/store/user/crovelli/zeeCalib/2012B/DoubleEG_bis/DoubleEG/crab_ZSkimData2012B/150713_203921/0000/zeeCalibSkim_11.root",
+"/store/user/crovelli/zeeCalib/2012B/DoubleEG_bis/DoubleEG/crab_ZSkimData2012B/150713_203921/0000/zeeCalibSkim_12.root",
+"/store/user/crovelli/zeeCalib/2012B/DoubleEG_bis/DoubleEG/crab_ZSkimData2012B/150713_203921/0000/zeeCalibSkim_13.root",
+"/store/user/crovelli/zeeCalib/2012B/DoubleEG_bis/DoubleEG/crab_ZSkimData2012B/150713_203921/0000/zeeCalibSkim_14.root",
+"/store/user/crovelli/zeeCalib/2012B/DoubleEG_bis/DoubleEG/crab_ZSkimData2012B/150713_203921/0000/zeeCalibSkim_15.root",
+"/store/user/crovelli/zeeCalib/2012B/DoubleEG_bis/DoubleEG/crab_ZSkimData2012B/150713_203921/0000/zeeCalibSkim_16.root",
+"/store/user/crovelli/zeeCalib/2012B/DoubleEG_bis/DoubleEG/crab_ZSkimData2012B/150713_203921/0000/zeeCalibSkim_1.root",
+"/store/user/crovelli/zeeCalib/2012B/DoubleEG_bis/DoubleEG/crab_ZSkimData2012B/150713_203921/0000/zeeCalibSkim_2.root",
+"/store/user/crovelli/zeeCalib/2012B/DoubleEG_bis/DoubleEG/crab_ZSkimData2012B/150713_203921/0000/zeeCalibSkim_3.root",
+"/store/user/crovelli/zeeCalib/2012B/DoubleEG_bis/DoubleEG/crab_ZSkimData2012B/150713_203921/0000/zeeCalibSkim_4.root",
+"/store/user/crovelli/zeeCalib/2012B/DoubleEG_bis/DoubleEG/crab_ZSkimData2012B/150713_203921/0000/zeeCalibSkim_5.root",
+"/store/user/crovelli/zeeCalib/2012B/DoubleEG_bis/DoubleEG/crab_ZSkimData2012B/150713_203921/0000/zeeCalibSkim_6.root",
+"/store/user/crovelli/zeeCalib/2012B/DoubleEG_bis/DoubleEG/crab_ZSkimData2012B/150713_203921/0000/zeeCalibSkim_7.root",
+"/store/user/crovelli/zeeCalib/2012B/DoubleEG_bis/DoubleEG/crab_ZSkimData2012B/150713_203921/0000/zeeCalibSkim_8.root",
+"/store/user/crovelli/zeeCalib/2012B/DoubleEG_bis/DoubleEG/crab_ZSkimData2012B/150713_203921/0000/zeeCalibSkim_9.root"
+
+])
