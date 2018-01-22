8baa617ad00c1fa2afd0192d941534a523a91384
crovelli <crovelli@gmail.com>
2018-01-22 10:33:32 +0100

    first commit: cut on rechit energy

--- RecoParticleFlow/PFClusterProducer/plugins/PFRecHitProducer.cc
+++ RecoParticleFlow/PFClusterProducer/plugins/PFRecHitProducer.cc
@@ -1,6 +1,8 @@
 #include "RecoParticleFlow/PFClusterProducer/plugins/PFRecHitProducer.h"
 #include "FWCore/Utilities/interface/RunningAverage.h"
 
+#include <iostream>
+
 namespace {
   bool sortByDetId(const reco::PFRecHit& a,
 		   const reco::PFRecHit& b) {
@@ -13,6 +15,7 @@ namespace {
 
  PFRecHitProducer:: PFRecHitProducer(const edm::ParameterSet& iConfig)
 {
+  //std::cout << "chiara: costruttore di PFRecHitProducer" << std::endl;
 
   produces<reco::PFRecHitCollection>();
   produces<reco::PFRecHitCollection>("Cleaned");
@@ -23,7 +26,9 @@ namespace {
   for (auto & creator : creators) {
       std::string name = creator.getParameter<std::string>("name");
       creators_.emplace_back(PFRecHitFactory::get()->create(name,creator,iC));
+      //std::cout << "chiara: dentro creators -> name = " << name << std::endl;
   }
+  //std::cout << "chiara: creators_.size = " << creators_.size() << std::endl;
 
 
   edm::ParameterSet navSet = iConfig.getParameter<edm::ParameterSet>("navigator");
@@ -42,9 +47,11 @@ namespace {
 
 void
  PFRecHitProducer::beginLuminosityBlock(edm::LuminosityBlock const& iLumi, const edm::EventSetup& iSetup) {
+  //std::cout << "chiara: dentro beginLuminosityBlock" << std::endl;
   for( const auto& creator : creators_ ) {
     creator->init(iSetup);
   }
+  //std::cout << "chiara: esco da beginLuminosityBlock" << std::endl;
 }
 
 void
@@ -54,25 +61,35 @@ void
 void
  PFRecHitProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
+  //std::cout << "chiara: dentro PFRecHitProducer::produce" << std::endl;
+
    using namespace edm;
    auto out = std::make_unique<reco::PFRecHitCollection>();
    auto cleaned = std::make_unique<reco::PFRecHitCollection>();
 
+   //std::cout << "chiara: dentro PFRecHitProducer::produce1" << std::endl;
    navigator_->beginEvent(iSetup);
+   //std::cout << "chiara: dentro PFRecHitProducer::produce2" << std::endl;
 
    out->reserve(localRA1.upper());
+   //std::cout << "chiara: dentro PFRecHitProducer::produce3" << std::endl;
    cleaned->reserve(localRA2.upper());
+   //std::cout << "chiara: dentro PFRecHitProducer::produce4" << std::endl;
 
    for( const auto& creator : creators_ ) {
+     //std::cout << "chiara: dentro PFRecHitProducer::produce => importRecHits" << std::endl;
      creator->importRecHits(out,cleaned,iEvent,iSetup);
+     //std::cout << "chiara: dentro PFRecHitProducer::produce => fatto importRecHits" << std::endl;
    }
 
+   //std::cout << "chiara, dentro PFRecHitProducer::produce5a = " << out->size() << std::endl;
    if (out->capacity()>2*out->size()) out->shrink_to_fit();
    if (cleaned->capacity()>2*cleaned->size()) cleaned->shrink_to_fit();
    localRA1.update(out->size());
    localRA2.update(cleaned->size());
    std::sort(out->begin(),out->end(),sortByDetId);
-
+   //std::cout << "chiara, dentro PFRecHitProducer::produce5b = " << out->size() << std::endl;
+ 
    //create a refprod here
    edm::RefProd<reco::PFRecHitCollection> refProd = 
      iEvent.getRefBeforePut<reco::PFRecHitCollection>();
